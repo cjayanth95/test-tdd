f0260af67200048970837795c5d66a26bb13f599
Jayanthculli <jayanthculli@GL-LAP-BLR-0544.local>
2022-09-26 09:16:02 +0530

    fix: fix desc

---
+++ release.config.js
@@ -0,0 +1,9 @@
+module.exports = {
+  branches: "main",
+  reporsitoryUrl: "https://github.com/cjayanth95/test-tdd",
+  plugins: [
+    "@semantic-release/commit-analyzer",
+    "@semantic-release/release-notes-generator",
+    "@semantic-release/github",
+  ],
+};
